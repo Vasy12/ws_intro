bc556a84b8fdaacb81663687cb0381690adb8901
Vasy Terkin <vasy.terkin12@gmail.com>
2020-03-25 19:19:39 +0200

    Create: simple WS usage

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = new WebSocket('ws://localhost:3000/');
+
+socket.onopen = function(event) {
+	alert('Connection open!');
+	console.dir(event);
+};
+
+socket.onclose = (event) => {
+	alert('Connection closed!');
+	console.dir(event);
+};
+
+socket.onmessage = (event) => {
+	console.dir(event.data);
+};
+
+class App extends Component{
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			messages: [],
+			messageValue: '',
+		};
+	}
+
+	sendMessage = () => {
+		socket.send(this.state.messageValue);
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			messageValue: e.target.value,
+		});
+	};
+
+	render() {
+
+		return (
+				<>
+					<input type="textarea" value={ this.state.messageValue }
+					       onChange={ this.handleChange }/>
+					<button onClick={ this.sendMessage }>Send message ></button>
+				</>
+		);
+	}
 }
 
 export default App;
--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 
 const app = express();
+const expressWs = require('express-ws')(app);
+const aWss = expressWs.getWss('/');
+app.use(express.json());
+
+app.ws('/', function(ws, req) {
+	ws.on('message', function(msg) {
+		console.log(msg);
+		aWss.clients.forEach(function(client) {
+			client.send(msg);
+		});
+	});
+	console.log('socket', req.testing);
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Example app listening on port ${ PORT }!`));
+
+
